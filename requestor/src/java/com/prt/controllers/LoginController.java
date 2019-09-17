c8cf58689c142763c132bbd266be08e4d804a927
Eric Pratt <pra17004@byui.edu>
2019-09-16 20:24:52 -0600

    Starting project

---
+++ requestor/src/java/com/prt/controllers/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prt.controllers;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author P-ratt
+ */
+@ManagedBean(name = "loginController")
+@ViewScoped
+public class LoginController implements Serializable {
+
+	@PostConstruct
+	void init() {
+
+	}
+
+	public String login() {
+		return "/requestor/requestor/main/dashboard.xhtml";
+	}
+}
